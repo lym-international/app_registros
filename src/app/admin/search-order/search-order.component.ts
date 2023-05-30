a4a9f01b92f4dfa013820f5081661d76408260c6
DiegoAlejandro <darm1980@gmail.com>
2023-05-30 13:13:34 -0500

    Front componente search-order

--- src/app/admin/search-order/search-order.component.ts
+++ src/app/admin/search-order/search-order.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+
 
 @Component({
   selector: 'app-search-order',
   templateUrl: './search-order.component.html',
-  styleUrls: ['./search-order.component.scss']
+  styleUrls: ['./search-order.component.scss'],
+  //standalone: true,
+  //imports: [],
 })
-export class SearchOrderComponent {
-
+export class SearchOrderComponent{
+  
 }
+  
+
--- src/app/admin/search-order/search-order.module.ts
+++ src/app/admin/search-order/search-order.module.ts
@@ -7,17 +7,26 @@ import { SharedModule } from '@shared';
 import { SearchOrderRoutingModule } from './search-order-routing.module';
 import { SearchOrderComponent } from './search-order.component';
 
+//material
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @NgModule({
-  declarations: [ SearchOrderComponent],
+  declarations: [SearchOrderComponent],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     ComponentsModule,
     SharedModule,
-    SearchOrderRoutingModule
+    SearchOrderRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
   ],
+  
   providers: [],
 })
 export class SearchOrderModule {}
