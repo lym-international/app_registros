76f55f3f9137cb5a69243175bbf9a1e0f3e52fcf
DiegoAlejandro <darm1980@gmail.com>
2023-06-05 12:32:57 -0500

    login

--- src/app/authentication/signin/signin.component.ts
+++ src/app/authentication/signin/signin.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import {
+  FormBuilder,
+  FormGroup,
   UntypedFormBuilder,
   UntypedFormGroup,
   Validators,
 } from '@angular/forms';
 import { Role, AuthService } from '@core';
 import { UnsubscribeOnDestroyAdapter } from '@shared';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http'; //Diego
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
@@ -16,30 +21,43 @@ export class SigninComponent
   extends UnsubscribeOnDestroyAdapter
   implements OnInit
 {
-  authForm!: UntypedFormGroup;
+  authForm!: FormGroup; //authForm!: UntypedFormGroup;
   submitted = false;
   loading = false;
   error = '';
   hide = true;
   constructor(
-    private formBuilder: UntypedFormBuilder,
+    private formBuilder: FormBuilder, //private formBuilder: UntypedFormBuilder,
     private route: ActivatedRoute,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private http: HttpClient          /*Diego*/
+
   ) {
     super();
   }
-
+  
   ngOnInit() {
+    
     this.authForm = this.formBuilder.group({
-      username: ['admin@software.com', Validators.required],
-      password: ['admin@123', Validators.required],
+      username: ['', Validators.required], //['admin@software.com', Validators.required],
+      password: ['', Validators.required], //['admin@123', Validators.required],
     });
   }
-  get f() {
+
+  //Diego
+  login() {
+    const username = this.authForm.get('username')?.value;
+    const password = this.authForm.get('password')?.value;
+  } 
+
+  /*get f() {
     return this.authForm.controls;
-  }
-  adminSet() {
+  }*/
+
+  
+
+/* adminSet() {
     this.authForm.get('username')?.setValue('admin@software.com');
     this.authForm.get('password')?.setValue('admin@123');
   }
@@ -50,16 +68,72 @@ export class SigninComponent
   clientSet() {
     this.authForm.get('username')?.setValue('client@software.com');
     this.authForm.get('password')?.setValue('client@123');
-  }
+  } */
   onSubmit() {
     this.submitted = true;
     this.loading = true;
     this.error = '';
     if (this.authForm.invalid) {
-      this.error = 'Username and Password not valid !';
+      this.error = 'Username or Password not valid !';
       return;
     } else {
-      this.subs.sink = this.authService
+        
+      const username = this.authForm.get('username')?.value;
+      const password = this.authForm.get('password')?.value;
+      console.log('Username:', username);
+      console.log('Password:', password);
+
+      
+      let formData = {
+        username: "", //username: "jairo@gmail.com",
+        password: "" // password: "jairo123"
+      };
+      
+      fetch(`https://us-central1-highkeystaff.cloudfunctions.net/auth/login`, {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => console.error(error));
+
+       this.subs.sink = this.authService //
+
+        .login(this.authForm.get('username')?.value, this.authForm.get('password')?.value)
+        .subscribe(
+          (res) => {
+            if (res) {
+              setTimeout(() => {
+                const role = this.authService.currentUserValue.role;
+                if (role === Role.All || role === Role.Admin) {
+                  this.router.navigate(['/admin/dashboard/main']);
+                } else if (role === Role.Employee) {
+                  this.router.navigate(['/employee/dashboard']);
+                } else if (role === Role.Client) {
+                  this.router.navigate(['/client/dashboard']);
+                } else {
+                  this.router.navigate(['/authentication/signin']);
+                }
+                this.loading = false;
+              }, 1000);
+            } else {
+              this.error = 'Invalid Login';
+            }
+          },
+          (error) => {
+            this.error = error;
+            this.submitted = false;
+            this.loading = false;
+          }
+        );   
+        
+        
+      /* this.subs.sink = this.authService
         .login(this.f['username'].value, this.f['password'].value)
         .subscribe(
           (res) => {
@@ -86,7 +160,8 @@ export class SigninComponent
             this.submitted = false;
             this.loading = false;
           }
-        );
-    }
+        );*/
+    } 
   }
 }
+
