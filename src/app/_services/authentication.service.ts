0b4a40ce800802a2b5c576eedd6c309fecaef569
lym-international <lyminternacional2023@gmail.com>
2024-01-29 10:46:40 -0500

    ajustes de commit

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -7,16 +7,14 @@ import firebase from 'firebase/compat/app';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { User } from 'app/_models/user';
 import { TranslateModule } from '@ngx-translate/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection,} from '@angular/fire/compat/firestore';
+import { getAuth, signInWithCustomToken } from 'firebase/auth';
 
 import { catchError } from 'rxjs/operators'; //Diego
 import { throwError } from 'rxjs'; //Diego
-
 import * as CryptoJS from 'crypto-js'; //Jairo
-
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
   private currentUserSubject: BehaviorSubject<User>;
@@ -34,150 +32,163 @@ export class AuthenticationService {
   private data: any;
   public currentUserData: any; // Nueva variable para almacenar los datos del usuario
   //private authentication: Auth;
-
-  private secretKey: 'jairo@adminl&m';
+  private secretKey = 'jairo@adminl&m';
+  
+  
   private isAuthenticatingSubject = new BehaviorSubject<boolean>(false);
   public isAuthenticating$ = this.isAuthenticatingSubject.asObservable();
 
   constructor(
-      private http: HttpClient,
-      public auth: AngularFireAuth,
-      private db: AngularFirestore,
-      // private db:
-      private router: Router,
-      //private messageService: MessageService,
-      private route: ActivatedRoute,
-      // private notifSvc: NotificationsService,
-      
-      ) {
-      this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/dashboard';
-      this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(sessionStorage.getItem('currentUser') || 'null'));
-      // this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(sessionStorage.getItem('currentUser')));
-      this.currentUser = this.currentUserSubject.asObservable();
-      
-      this.auth.authState.subscribe(user => {
-        if (!user) {
-          sessionStorage.setItem('currentUser', '');
-        }
-      });
+    private http: HttpClient,
+    public auth: AngularFireAuth,
+    private db: AngularFirestore,
+    // private db:
+    private router: Router,
+    //private messageService: MessageService,
+    private route: ActivatedRoute
+    // private notifSvc: NotificationsService,
+  ){
+    this.returnUrl =
+      this.route.snapshot.queryParams['returnUrl'] || '/dashboard';
+    this.currentUserSubject = new BehaviorSubject<User>(
+      JSON.parse(sessionStorage.getItem('currentUser') || 'null')
+    );
+    // this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(sessionStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
 
-      //const authen = new Auth();
-      //this.authentication = authen;
-    }
-    
-    decryptData(encryptedData: string): string {
-      try {
-        const decryptedData = CryptoJS.AES.decrypt(
-          encryptedData,
-          this.secretKey
-        ).toString(CryptoJS.enc.Utf8);
-        return decryptedData;
-      } catch (error) {
-        console.error('Error :', error);
-        return null;
+    this.auth.authState.subscribe((user) => {
+      if (!user) {
+        sessionStorage.setItem('currentUser', '');
       }
-    }
+    });
   
-    private extractOriginalString(alteredString: string): string {
-      const parts = alteredString.split('_');
-      if (parts.length >= 3) {
-        return parts[1];
-      } else {
-        return alteredString;
-      }
-    }
-    initialize() {
-      this.route.queryParams.subscribe((params) => {
-        const token = params['token'];
-        if (token) {
-          this.isAuthenticatingSubject.next(true);
-          // console.log("token", token);
-          // const tk = token.replace(/["']/g, '');
-          // console.log("cirpted", tk);
-          
-          const receivedToken = token.replace(/-/g, '+').replace(/_/g, '/');
-          const decryptedToken = this.decryptData(receivedToken);
-          // const decryptedToken = this.decryptData(tk);
-          if (decryptedToken) {
-            try {
-              const userData = JSON.parse(decryptedToken);
-              const username = userData.username;
-              const password = userData.pass;
-              const pass = this.extractOriginalString(password);
-             
-              this.login(username, pass);
-              
-            } catch (error) {
-              console.error('Error al analizar la cadena desencriptada:', error);
-            }
-          } else {
-            console.error('La cadena desencriptada es nula o vacía.');
-          }
-        }
-      })
-    }
+  }
 
-    login(username: string, password: string ) {
-      this.auth.signInWithEmailAndPassword(username, password).then((user) => {
-        console.log("usuario autenticado con exito!", user.user?.email)
-        this.db.collection('Users',ref => ref.where('email', '==', username)).get().subscribe((usersInfo) => {
-          
-          usersInfo.docs.forEach((item:any) => {
-            const data = item.data();
-            sessionStorage.setItem('currentUser', JSON.stringify(data));
-            this.currentUserSubject.next(data);
-            this.currentUserData = data; // diego 8-7 : Almacenar los datos del usuario en currentUserData
-            localStorage.setItem('currentUserData', JSON.stringify(data));
-            console.log('Datos del usuario almacenados en localStorage:', data);
-            console.log('currentUserData: ', this.currentUserData.firstname)
-            console.log("nombre: ", data.firstname, data.lastname)
-            console.log("role: ", data.role)
+  decryptData(encryptedData: string): string {
+    try {
+      const decryptedData = CryptoJS.AES.decrypt(
+        encryptedData,
+        this.secretKey
+      ).toString(CryptoJS.enc.Utf8);
+      return decryptedData;
+    } catch (error) {
+      console.error('Error :', error);
+      return null;
+    }
+  }
+
+  private extractOriginalString(alteredString: string): string {
+    const parts = alteredString.split('_');
+    if (parts.length >= 3) {
+      return parts[1];
+    } else {
+      return alteredString;
+    }
+  }
+  initialize() {
+    this.route.queryParams.subscribe((params) => {
+      const token = params['token'];
+      if (token) {
+        this.isAuthenticatingSubject.next(true);
+        // console.log("token", token);
+        // const tk = token.replace(/["']/g, '');
+        // console.log("cirpted", tk);
+        
+        const receivedToken = token.replace(/-/g, '+').replace(/_/g, '/');
+        const decryptedToken = this.decryptData(receivedToken);
+        // const decryptedToken = this.decryptData(tk);
+        if (decryptedToken) {
+          try {
+            const userData = JSON.parse(decryptedToken);
+            const username = userData.username;
+            const password = userData.pass;
+            const pass = this.extractOriginalString(password);
+           
+            this.login(username, pass);
             
-            this.setData(data)
-            // this.auxCurrentUser = user;
-            // const auxDate = new Date();
-            // const date = new Date(auxDate.getTime() - (auxDate.getTimezoneOffset() * 60000));
-            setTimeout(() => {
-              this.isAuthenticatingSubject.next(false);
-            }, 1000);
-            if(this.currentUserData.role=="Employee"){
-              console.log("Es Empleado")
-              this.router.navigate(['/admin/search-order']);
-            } else if (this.currentUserData.role=="Client"){
-                console.log("Es cliente")
+          } catch (error) {
+            console.error('Error al analizar la cadena desencriptada:', error);
+          }
+        } else {
+          this.isAuthenticatingSubject.next(false);
+          console.error('La cadena desencriptada es nula o vacía.');
+        }
+      }
+    })
+  }
+  login(username: string, password: string) {
+   /*  setTimeout(() => {
+      this.isAuthenticatingSubject.next(false);
+    }, 1500); */
+    this.auth
+      .signInWithEmailAndPassword(username, password)
+      .then((user) => {
+        // console.log('usuario autenticado con exito!', user.user?.email);
+
+        this.db
+          .collection('Users', (ref) => ref.where('email', '==', username))
+          .get()
+          .subscribe((usersInfo) => {
+            usersInfo.docs.forEach((item: any) => {
+              const data = item.data();
+              // console.log('DATA ::', item.data());
+              sessionStorage.setItem('currentUser', JSON.stringify(data));
+              this.currentUserSubject.next(data);
+              this.currentUserData = data; // diego 8-7 : Almacenar los datos del usuario en currentUserData
+              localStorage.setItem('currentUserData', JSON.stringify(data));
+
+              this.setData(data);
+              setTimeout(() => {
+                this.isAuthenticatingSubject.next(false);
+              }, 1000);
+              // this.auxCurrentUser = user;
+              // const auxDate = new Date();
+              // const date = new Date(auxDate.getTime() - (auxDate.getTimezoneOffset() * 60000));
+
+              if (this.currentUserData.role == 'Employee') {
+                console.log('Es Empleado');
+                this.router.navigate(['/admin/search-order']);
+              } else if (this.currentUserData.role == 'Client') {
+                console.log('Es cliente');
                 this.router.navigate(['/admin/search-order/']);
-            } else if (this.currentUserData.role=='Executive'){
-                console.log("Executive")
-            } else if (this.currentUserData.role == "Supervisor"){
-                console.log("Es Supervisor")
+              } else if (this.currentUserData.role == 'Executive') {
+                console.log('Executive');
+              } else if (this.currentUserData.role == 'Supervisor') {
+                console.log('Es Supervisor');
                 this.router.navigate(['/admin/search-order/']);
-            } else if (this.currentUserData.role=="Administrator"){
-                console.log("Es administrador")
+              } else if (this.currentUserData.role == 'Administrator') {
+                console.log('Es administrador');
                 this.router.navigate(['/admin/search-order/']);
-            } else {
-              this.router.navigate(['/authentication/signin']); 
-            }
-          });
-        });
-      })
-            .catch((error) => {
-              this.isAuthenticatingSubject.next(false);
-              switch (error.code) {
-                case "auth/wrong-password":
-                  console.log("error1", error.message);
-                  this.messageService.messageWarning("Warning","The password is invalid or the user does not have a password.");
-                  break;
-                case "auth/too-many-requests":
-                  console.log("error2", error.message);
-                  this.messageService.messageWarning("Warning","Too many unsuccessful login attempts. Please try again later.");
-                  break;
-                default:
-                  console.log("error3", error.message);
-                  this.messageService.messageWarning("Warning", error.message);
-                  break;
+              } else {
+                this.router.navigate(['/authentication/signin']);
               }
             });
-    }
+          });
+      })
+      .catch((error) => {
+        this.isAuthenticatingSubject.next(false);
+        switch (error.code) {
+          case 'auth/wrong-password':
+            console.log('error1', error.message);
+            this.messageService.messageWarning(
+              'Warning',
+              'The password is invalid or the user does not have a password.'
+            );
+            break;
+          case 'auth/too-many-requests':
+            console.log('error2', error.message);
+            this.messageService.messageWarning(
+              'Warning',
+              'Too many unsuccessful login attempts. Please try again later.'
+            );
+            break;
+          default:
+            console.log('error3', error.message);
+            this.messageService.messageWarning('Warning', error.message);
+            break;
+         }
+    });
+  }
 
     setData(data: any) {
       this.data = data;
